224ad07ee9e9d06f595a28145cb1269ed3d88549
xrstf <git@xrstf.de>
2024-03-10 01:14:28 +0100

    [semver] prepare for Rudi 0.8, get rid of references to Reader/Writer interfaces

--- semver/types.go
+++ semver/types.go
@@ -4,15 +4,11 @@
 package semver
 
 import (
-	"fmt"
-	"strings"
-
 	blangsemver "github.com/blang/semver/v4"
 
 	"go.xrstf.de/rudi/pkg/coalescing"
 	"go.xrstf.de/rudi/pkg/deepcopy"
 	"go.xrstf.de/rudi/pkg/equality"
-	"go.xrstf.de/rudi/pkg/jsonpath"
 )
 
 type Semver struct {
@@ -20,69 +16,10 @@ type Semver struct {
 }
 
 var (
-	_ jsonpath.ObjectReader            = Semver{}
-	_ jsonpath.ObjectWriter            = Semver{}
 	_ deepcopy.Copier                  = Semver{}
 	_ coalescing.CustomStringCoalescer = Semver{}
 	_ equality.Comparer                = Semver{}
 )
-
-// GetObjectKey implements jsonpath.ObjectWriter.
-func (v Semver) GetObjectKey(name string) (any, error) {
-	switch strings.ToLower(name) {
-	case "major":
-		return int64(v.Version.Major), nil
-	case "minor":
-		return int64(v.Version.Minor), nil
-	case "patch":
-		return int64(v.Version.Patch), nil
-	default:
-		return nil, fmt.Errorf("unknown property %q", name)
-	}
-}
-
-func toInteger(val any) (int64, bool) {
-	switch asserted := val.(type) {
-	case int:
-		return int64(asserted), true
-	case int32:
-		return int64(asserted), true
-	case int64:
-		return asserted, true
-	case uint64:
-		return int64(asserted), true
-	default:
-		return 0, false
-	}
-}
-
-// SetObjectKey implements jsonpath.ObjectWriter.
-func (v Semver) SetObjectKey(name string, value any) (any, error) {
-	switch strings.ToLower(name) {
-	case "major":
-		val, ok := toInteger(value)
-		if !ok {
-			return nil, fmt.Errorf("cannot set major version to %T", value)
-		}
-		v.Version.Major = uint64(val)
-	case "minor":
-		val, ok := toInteger(value)
-		if !ok {
-			return nil, fmt.Errorf("cannot set minor version to %T", value)
-		}
-		v.Version.Minor = uint64(val)
-	case "patch":
-		val, ok := toInteger(value)
-		if !ok {
-			return nil, fmt.Errorf("cannot set patch version to %T", value)
-		}
-		v.Version.Patch = uint64(val)
-	default:
-		return nil, fmt.Errorf("unknown property %q", name)
-	}
-
-	return v, nil
-}
 
 // DeepCopy implements deepcopy.Copier.
 func (v Semver) DeepCopy() (any, error) {
